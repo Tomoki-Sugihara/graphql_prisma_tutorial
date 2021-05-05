9f57d3f88fc9b34aefde0efca453b77aefa31c09
sugihara10969 <59116409+sugihara10969@users.noreply.github.com>
2021-05-05 10:11:33 +0900

    Add connect docker-posgresql with graphql

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -2,14 +2,48 @@ import { objectType, queryType, mutationType, makeSchema } from '@nexus/schema'
 import { nexusPrisma } from 'nexus-plugin-prisma'
 import path from 'path'
 
+const User = objectType({
+  name: 'User',
+  definition(t) {
+    t.model.id()
+    t.model.name()
+  }
+})
+
 const Query = queryType({
   definition(t) {
-    t.string('hello', { resolve: () => 'hello world'})
+    t.list.field('allUsers', {
+      type: 'User',
+      resolve(_parent, _arg, ctx) {
+        return ctx.prisma.user.findMany({})
+      }
+    })
+
+    t.crud.user()
+    t.crud.users()
+  }
+})
+
+const Mutation = mutationType({
+  definition(t) {
+    t.field('bigRedButton', {
+      type: 'String',
+      async resolve(_parent, _arg, ctx) {
+        const { count } = await ctx.prisma.user.deleteMany({})
+        return `${count} user(s) destroyed.`
+      }
+    })
+
+    t.crud.createOneUser()
+    t.crud.deleteOneUser()
+    t.crud.deleteMany()
+    t.crud.updateOneUser()
+    t.crud.updateManyUser()
   }
 })
 
 export const schema = makeSchema({
-  types: [Query],
+  types: [User, Query, Mutation],
   plugins: [nexusPrisma({ experimentalCRUD: true })],
   outputs: {
     typegen: path.join(process.cwd(), 'generated', 'nexus-typegen.ts'),
